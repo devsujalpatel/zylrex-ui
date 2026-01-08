1e2e05f68316fb58b8d697221488c580db485772
devsujalpatel <sujal7455@gmail.com>
2026-01-08 23:47:40 +0530

    error: switch not rendering

--- src/components/switch.tsx
+++ src/components/switch.tsx
@@ -1,8 +1,12 @@
-import { mergeProps, Switch, useRender } from "@base-ui-components/react";
-import { cva, VariantProps } from "class-variance-authority";
+import { mergeProps, Switch, type useRender } from "@base-ui-components/react";
+import { cva, type VariantProps } from "class-variance-authority";
 
-const switchVariants = cva("cursor-pointer h-5.5 w-12 rounded-full focus-visible:outline-none focus-visible:ring-offset-2 focus-visible:ring-foreground focus-visible:ring-2 bg-background shadow-raised");
-const thumbVariants = cva();
+const switchVariants = cva(
+  "cursor-pointer h-5.5 w-12 rounded-full focus-visible:outline-none focus-visible:ring-offset-2 focus-visible:ring-foreground focus-visible:ring-2 bg-background shadow-inset data-checked:bg-primary"
+);
+const thumbVariants = cva(
+  "h-5 w-5 bg-background shadow-inset rounded-full block translate-x-0 data-checked:translate-x-6.5 relative"
+);
 
 interface SwitchProps
   extends useRender.ComponentProps<"switch">,
