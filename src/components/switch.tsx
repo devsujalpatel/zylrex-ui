9b9409ffd6e1012809ea9d86d59d72745eb7e67b
devsujalpatel <sujal7455@gmail.com>
2026-01-09 18:26:10 +0530

    fixed switch not showing

--- src/components/switch.tsx
+++ src/components/switch.tsx
@@ -2,10 +2,10 @@ import { mergeProps, Switch, type useRender } from "@base-ui-components/react";
 import { cva, type VariantProps } from "class-variance-authority";
 
 const switchVariants = cva(
-  "cursor-pointer h-5.5 w-12 rounded-full focus-visible:outline-none focus-visible:ring-offset-2 focus-visible:ring-foreground focus-visible:ring-2 bg-background shadow-inset data-checked:bg-primary"
+  "inline-flex items-center cursor-pointer h-5.5 w-12 rounded-full focus-visible:outline-none focus-visible:ring-offset-2 focus-visible:ring-foreground focus-visible:ring-2 bg-background shadow-inset data-checked:bg-primary"
 );
 const thumbVariants = cva(
-  "h-5 w-5 bg-background shadow-inset rounded-full block translate-x-0 data-checked:translate-x-6.5 relative"
+  "h-5 w-5 bg-background shadow-inset rounded-full block ease-out transition-all duration-350 translate-x-0 after:w-1 after:h-1 after:absolute after:top-1/2 after:left-1/2 after:-translate-1/2 after:bg-muted-foreground data-checked:after:bg-primary data-checked:translate-x-6.5 relative border border-highlight after:rounded-full"
 );
 
 interface SwitchProps
